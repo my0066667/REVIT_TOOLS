caad7e3511e43b5b7bf73f9e850af046658fc521
my0066667 <tranmy290404@gmail.com>
2026-07-08 17:48:30 +0700

    up

---
+++ WpfApp1/WpfApp1/Commands/ClassifyRebarLocationCommand.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1.Commands
+{
+    [Transaction(TransactionMode.Manual)]
+    internal class ClassifyRebarLocationCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApplication = commandData.Application;
+            UIDocument uiDocument = uiApplication.ActiveUIDocument;
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
