9136bdc959573509d0ca73a64de705c3e05073d7
my0066667 <tranmy290404@gmail.com>
2026-07-09 17:35:52 +0700

    up

--- WpfApp1/WpfApp1/Commands/Model/CreateSlabAnnotateModel.cs
+++ WpfApp1/WpfApp1/Commands/Model/CreateSlabAnnotateModel.cs
@@ -1,10 +1,21 @@
-﻿using System;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WpfApp1.Commands.Model
 {
-    internal class CreateSlabAnnotateModel
+    public class FloorData
     {
+        public Floor FloorElement { get; set; }
+        public Transform LinkTransform { get; set; } // Transform của file link chứa sàn này
+        public string SourceName { get; set; }        // Tên file nguồn (dùng để debug nếu cần)
+
+        public void cal()
+        {
+            // Placeholder for any calculations or methods related to FloorData
+        }
     }
+
 }
